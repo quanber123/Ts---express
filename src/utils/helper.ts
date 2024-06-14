875819e861c0637cc8983d6a2bcad928d156d599
quanber123 <quantm103@gmail.com>
2024-06-14 23:15:41 +0700

    [feat] add delete file

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,4 +1,6 @@
 import diacritics from 'diacritics';
+import fs from 'fs';
+
 export const totalPage = (totalItems: number, limit: number) => {
   return Math.ceil(totalItems / limit);
 };
@@ -9,4 +11,21 @@ export const formatDiacritics = (str: string) => {
 
 export const formatRegex = (str: string) => {
   return new RegExp(formatDiacritics(str), 'i');
+};
+
+export const deleteFile = async (path: string) => {
+  fs.access(path, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error('Tệp tin không tồn tại hoặc không thể truy cập.');
+      return;
+    }
+
+    fs.unlink(path, (err) => {
+      if (err) {
+        console.error('Lỗi khi xóa tệp tin:', err);
+        return;
+      }
+      console.log('Tệp tin đã được xóa thành công.');
+    });
+  });
 };
